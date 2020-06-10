9ca20e6ac535cafcbc1bc92581419d204a7407ad
Pascal Grobecker <pascal.grobecker@unibas.ch>
2020-06-10 15:12:14 +0200

    made ete3 optional

--- cellstates/__init__.py
+++ cellstates/__init__.py
@@ -1,5 +1,4 @@
 from .cluster import Cluster
-from .run import run_mcmc
 from .helpers import clusters_from_hierarchy, get_hierarchy_df, get_scipy_hierarchy, hierarchy_to_newick
 from .helpers import marker_score_table
 from .plotting import plot_hierarchy_scipy
@@ -7,3 +6,4 @@ try:
     from .plotting import plot_hierarchy_ete3
 except ImportError:
     pass
+from .run import run_mcmc
