cf93c515fd11d8b1f66a7fe0a21757537dfc06e1
Pascal Grobecker <pascal.grobecker@unibas.ch>
2022-06-24 02:39:38 +0200

    fix: add_dataset now works if set of genes is different

--- scripts/add_dataset.py
+++ scripts/add_dataset.py
@@ -38,12 +38,20 @@ def main():
                         help='directory for new output', type=str)
     parser.add_argument('-g', '--genes', default=None,
                         help='gene names (path to file)', type=str)
+    parser.add_argument('--new-genes', default=None, dest='new_genes',
+                        help='gene names (path to file) of new data if different', type=str)
     parser.add_argument('-c', '--cells', default=None, nargs='*',
                         help='cell names/barcodes (path to file)', type=str)
     parser.add_argument('-t', '--threads', default=1,
                         help='number of threads', type=int)
     parser.add_argument('-s', '--seed', default=1,
                         help='seed for random generator', type=int)
+    parser.add_argument('--save-intermediates', dest='save_intermediates',
+                        action='store_true',
+                        help='regularly save intermediate results')
+    parser.add_argument('--dirichlet-file', default=None, dest='dirichlet_file',
+                        help='dirichlet prior parameter file from previous run',
+                        type=str)
     parser.set_defaults(optimize_prior=True)
     args = parser.parse_args()
 
@@ -51,10 +59,24 @@ def main():
 
     # iterate over datafiles and load them
     # data and cellnames are added to lists for concatenating
-    all_data = []
-    all_cells = []
-    genes = None
-    for datafiles in (args.old_data, args.new_data):
+    all_df = []
+
+    # gene names for old dataset
+    if args.genes is not None:
+        old_genes = np.loadtxt(args.genes, dtype=str, delimiter='\n')
+    else:
+        old_genes = None
+
+    # gene names for new dataset
+    if args.new_genes is not None:
+        new_genes = np.loadtxt(args.new_genes, dtype=str, delimiter='\n')
+    elif old_genes is not None:
+        # assume genes are the same if not specified otherwise
+        new_genes = old_genes
+    else:
+        new_genes = None
+
+    for datafiles, genes in ((args.old_data, old_genes), (args.new_data, new_genes)):
         index = 0
         for datafile in datafiles:
             logging.info(f'loading {datafile}')
@@ -68,48 +90,52 @@ def main():
                     # if above fails, use slower method and infer delimiter
                     df = pd.read_csv(datafile, sep=None, header=0, index_col=0,
                                     engine='python')
-                genes = df.index.values
-                cells = df.columns.values
-                data = df.values
             elif filetype=='npy':
                 data = np.load(datafile)
                 cells = [f'{filename}-cell_{i}' for i in range(data.shape[1])]
+                df = pd.DataFrame(data, index=genes, columns=cells)
             elif filetype=='mtx':
                 import scipy.io as sio
                 data = sio.mmread(datafile).toarray()
                 cells = [f'{filename}-cell_{i}' for i in range(data.shape[1])]
+                df = pd.DataFrame(data, index=genes, columns=cells)
             else:
                 raise ValueError('filetype not recognized', datafile)
 
-            # round to nearest int and cast to np.int64 [long] dtype
-            if np.issubdtype(data.dtype, np.floating):
-                data = np.rint(data, out=data)
-            data = data.astype(np.int64, copy=False)
             index += data.shape[1]
 
-            all_data.append(data)
-            all_cells.append(cells)
+            all_df.append(df)
+
+        # remember which datasets were clustered previously
         if datafiles == args.old_data:
             min_index = index
 
     # build final data table and cell list
-    data = np.concatenate(all_data, axis=1)
+    df = pd.concat(all_df, axis=1).fillna(0)
+
     if args.cells is not None:
         all_cells = []
         for cellfile in args.cells:
             all_cells.append(np.loadtxt(cellfile, dtype=str))
-    cells = np.concatenate(all_cells)
+        cells = np.concatenate(all_cells)
+    else:
+        all_cells = df.columns.values
+    genes = df.index.values
 
-    if args.genes is not None:
-        genes = np.loadtxt(args.genes, dtype=str, delimiter='\n')
-    elif genes is None:
-        genes = np.arange(data.shape[0], dtype=int)
+    # round to nearest int and cast to np.int64 [long] dtype
+    data = df.values
+    if np.issubdtype(data.dtype, np.floating):
+        data = np.rint(data, out=data)
+    data = data.astype(np.int64, copy=False)
 
     # -------- initialise model parameters -------- #
 
     # construct initial array of Dirichlet pseudocounts with scale alpha
-    alpha = np.loadtxt(os.path.join(args.resultsdir,
-                                    'dirichlet_pseudocounts.txt')).sum()
+    if args.dirichlet_file is not None:
+        alpha = np.loadtxt(args.dirichlet_file).sum()
+    else:
+        alpha = np.loadtxt(os.path.join(args.resultsdir,
+                                        'dirichlet_pseudocounts.txt')).sum()
     # whether pseudocounts are optimized
     find_best_alpha = args.optimize_prior
 
@@ -124,20 +150,30 @@ def main():
     G, N = data.shape
 
     # construct initial cluster array
-    cluster_init = np.loadtxt(os.path.join(args.resultsdir,
-                                           'optimized_clusters.txt'), dtype=int)
-    a = np.max(cluster_init) + 1
-    b = N - min_index + a
-    cluster_init = np.concatenate([cluster_init, np.arange(a, b, dtype=int)])
+    if args.init is None:
+        cluster_init = np.loadtxt(os.path.join(args.resultsdir,
+                                            'optimized_clusters.txt'), dtype=int)
+        a = np.max(cluster_init) + 1
+        b = N - min_index + a
+        cluster_init = np.concatenate([cluster_init, np.arange(a, b, dtype=int)])
+    else:
+        # assume initialisation is a file from a previous run
+        cluster_init = np.loadtxt(args.init, dtype=int)
 
     clst = Cluster(data, LAMBDA, cluster_init.copy(), max_clusters=b+1,
                    num_threads=args.threads, n_cache=N_CACHE, seed=args.seed)
 
     # -------- run optimization algorithm -------- #
 
-    # estimate total runtime
+    if args.save_intermediates:
+        dirichlet_file = os.path.join(args.outdir, 'dirichlet_pseudocounts.txt')
+        np.savetxt(dirichlet_file, clst.dirichlet_pseudocounts)
+        intermediate_dir = args.outdir
+    else:
+        intermediate_dir = None
+
     run_mcmc(clst, N_steps=N, log_level=LOG_LEVEL, tries_per_step=TPS,
-             min_index=min_index)
+             results_dir=intermediate_dir, min_index=min_index)
 
     # -------- Save outputs of model -------- #
 
